0195cfbb1610b065b72ecfaa65f998f8ec8fbceb
Yan Soares Couto <yancouto@gmail.com>
2023-11-08 12:54:55 -0300

    Change dictionaries to not use str

--- project/game/level/cells/RegularCell.gd
+++ project/game/level/cells/RegularCell.gd
@@ -5,30 +5,30 @@ const DIAGONAL_BUTTON_MASK = preload("res://assets/images/ui/diagonal_button_mas
 signal pressed
 
 @onready var Waters = {
-	"s" : $Waters/Single,
-	"tl" : $Waters/TopLeft,
-	"tr" : $Waters/TopRight,
-	"bl" : $Waters/BottomLeft,
-	"br" : $Waters/BottomRight,
+	E.Waters.Single: $Waters/Single,
+	E.Waters.TopLeft: $Waters/TopLeft,
+	E.Waters.TopRight: $Waters/TopRight,
+	E.Waters.BottomLeft: $Waters/BottomLeft,
+	E.Waters.BottomRight: $Waters/BottomRight,
 }
 @onready var Buttons = {
-	"s" : $Buttons/Single,
-	"tl" : $Buttons/TopLeft,
-	"tr" : $Buttons/TopRight,
-	"bl" : $Buttons/BottomLeft,
-	"br" : $Buttons/BottomRight,
+	E.Single: $Buttons/Single,
+	E.TopLeft: $Buttons/TopLeft,
+	E.TopRight: $Buttons/TopRight,
+	E.BottomLeft: $Buttons/BottomLeft,
+	E.BottomRight: $Buttons/BottomRight,
 }
 @onready var Hints = {
-	"dec": $DiagHints/Dec,
-	"inc": $DiagHints/Inc,
+	E.Dec: $DiagHints/Dec,
+	E.Inc: $DiagHints/Inc,
 }
 @onready var Walls = {
-	"top": $Walls/Top,
-	"right": $Walls/Right,
-	"bottom": $Walls/Bottom,
-	"left":$Walls/Left,
-	"dec": $Walls/DecDiag,
-	"inc": $Walls/IncDiag,
+	E.Walls.Top: $Walls/Top,
+	E.Walls.Right: $Walls/Right,
+	E.Walls.Bottom: $Walls/Bottom,
+	E.Walls.Left:$Walls/Left,
+	E.Walls.DecDiag: $Walls/DecDiag,
+	E.Walls.IncDiag: $Walls/IncDiag,
 }
 
 var row : int
@@ -48,35 +48,21 @@ func setup(type : E.CellType, i : int, j : int) -> void:
 		wall.hide()
 	match type:
 		E.CellType.Single:
-			Buttons.s.show()
+			Buttons[E.Single].show()
 		E.CellType.IncDiag:
-			Buttons.tl.show()
-			Buttons.br.show()
+			Buttons[E.TopLeft].show()
+			Buttons[E.BottomRight].show()
 			set_wall(E.Walls.IncDiag)
 		E.CellType.DecDiag:
-			Buttons.tr.show()
-			Buttons.bl.show()
+			Buttons[E.TopRight].show()
+			Buttons[E.BottomLeft].show()
 			set_wall(E.Walls.DecDiag)
 		_:
 			push_error("Not a valid type of cell:" + str(type))
 
 
 func set_wall(wall : E.Walls) -> void:
-	match wall:
-		E.Walls.Top:
-			Walls.top.show()
-		E.Walls.Right:
-			Walls.right.show()
-		E.Walls.Bottom:
-			Walls.bottom.show()
-		E.Walls.Left:
-			Walls.left.show()
-		E.Walls.IncDiag:
-			Walls.inc.show()
-		E.Walls.DecDiag:
-			Walls.dec.show()
-		_:
-			push_error("Not a valid wall:" + str(wall))
+	Walls[wall].show()
 
 
 func remove_water():
@@ -86,20 +72,10 @@ func remove_water():
 
 func set_water(water : E.Waters, value: bool) -> void:
 	match water:
-		E.Waters.Single:
-			Waters.s.visible = value
-		E.Waters.TopLeft:
-			Waters.tl.visible = value
-		E.Waters.TopRight:
-			Waters.tr.visible = value
-		E.Waters.BottomRight:
-			Waters.br.visible = value
-		E.Waters.BottomLeft:
-			Waters.bl.visible = value
 		E.Waters.None:
 			remove_water()
 		_:
-			push_error("Not a valid water:" + str(water))
+			Waters[water].visible = value
 
 
 func _on_button_pressed(which : String) -> void:
