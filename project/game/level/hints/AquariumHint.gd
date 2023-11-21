514951c3f85b58fa5fefecd71689dff32464c74d
Yan Couto <yancouto@gmail.com>
2023-11-21 19:33:59 -0300

    Fix level buttons

--- project/game/level/hints/AquariumHint.gd
+++ project/game/level/hints/AquariumHint.gd
@@ -7,8 +7,8 @@ func startup(delay: float) -> void:
 	await get_tree().create_timer(delay).timeout
 	AnimPlayer.play("startup")
 
-func set_values(size: float, amount: int, current: int, editor_mode: bool) -> void:
-	$Size.text = str(size)
+func set_values(aq_size: float, amount: int, current: int, editor_mode: bool) -> void:
+	$Size.text = str(aq_size)
 	$ExpectedAmount.text = "x%d" % amount
 	$CurrentAmount.text = "(%d)" % current
 	if current < amount or editor_mode:
