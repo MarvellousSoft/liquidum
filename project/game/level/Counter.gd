fee11651bf18abcdb0a590aa7c8c335e79245c97
Ricardo Lira <pudimdefrutas@gmail.com>
2023-11-21 17:30:39 -0300

    Adding visibility button to counters

--- project/game/level/Counter.gd
+++ project/game/level/Counter.gd
@@ -1,16 +1,40 @@
 extends Control
 
+const ALPHA_SPEED = 4.0
+const HIDE_ALPHA = 0.5
+
 @export var counter_name := "MISTAKES_COUNTER"
 @export var check_for_satisfied := true
 
+@onready var VisibilityButton = %VisibilityButton
 @onready var Counter = $HBoxContainer/Counter
 @onready var MainLabel = $HBoxContainer/Label
 @onready var AnimPlayer = $AnimationPlayer
 
 var count: float = 0.0
+var visibility_active := true
+
 
 func _ready():
+	disable_editor()
 	MainLabel.text = tr(counter_name)
+
+
+func _process(dt):
+	if visibility_active:
+		MainLabel.modulate.a = min(MainLabel.modulate.a + ALPHA_SPEED*dt, 1.0)
+		Counter.modulate.a = min(Counter.modulate.a + ALPHA_SPEED*dt, 1.0)
+	else:
+		MainLabel.modulate.a = max(MainLabel.modulate.a - ALPHA_SPEED*dt, HIDE_ALPHA)
+		Counter.modulate.a = max(Counter.modulate.a - ALPHA_SPEED*dt, HIDE_ALPHA)
+
+
+func enable_editor():
+	VisibilityButton.show()
+
+
+func disable_editor():
+	VisibilityButton.hide()
 
 
 func startup(delay : float) -> void:
@@ -43,3 +67,7 @@ func update_label() -> void:
 			Counter.add_theme_color_override("font_color", Global.COLORS.normal)
 		else:
 			Counter.add_theme_color_override("font_color", Global.COLORS.error)
+
+
+func _on_visibility_button_toggled(button_pressed):
+	visibility_active = button_pressed
