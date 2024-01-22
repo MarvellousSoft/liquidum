fa2e24cb0decb1b39573be806da263d14f916476
Yan Couto <yancouto@gmail.com>
2024-01-22 19:07:50 -0300

    Our other games button on credits

--- project/game/credits/CreditsScreen.gd
+++ project/game/credits/CreditsScreen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const GAMES_LINK := "https://store.steampowered.com/search/?publisher=Marvellous%20Soft"
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"return"):
 		TransitionManager.pop_scene()
@@ -12,3 +14,10 @@ func _on_game_pressed(app_id: int) -> void:
 
 func _on_back_pressed() -> void:
 	TransitionManager.pop_scene()
+
+
+func _on_other_games_pressed():
+	if SteamManager.enabled:
+		SteamManager.steam.activateGameOverlayToWebPage(GAMES_LINK)
+	else:
+		OS.shell_open(GAMES_LINK)
