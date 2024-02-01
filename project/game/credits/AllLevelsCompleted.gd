cac614edc0c35322d84b292b52b23c6285c079b1
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-01 19:25:00 -0300

    More sfx pass

--- project/game/credits/AllLevelsCompleted.gd
+++ project/game/credits/AllLevelsCompleted.gd
@@ -7,9 +7,14 @@ func _ready():
 
 
 func _on_continue_pressed():
+	AudioManager.play_sfx("button_pressed")
 	TransitionManager.change_scene(Global.load_mobile_compat("res://game/credits/CreditsScreen").instantiate())
 
 
 func _on_dark_mode_changed(is_dark : bool):
 	theme = Global.get_theme(is_dark)
 	%MarginContainer.theme = Global.get_font_theme(is_dark)
+
+
+func _on_button_mouse_entered():
+	AudioManager.play_sfx("button_hover")
