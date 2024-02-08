c98637cec0abd466199de615987d180bf836b6a0
Yan Soares Couto <yancouto@gmail.com>
2024-02-08 19:15:35 -0300

    Workshop vote buttons working

--- project/game/workshop_menu/WorkshopLevelButton.gd
+++ project/game/workshop_menu/WorkshopLevelButton.gd
@@ -32,9 +32,9 @@ func load_level() -> LevelData:
 func get_vote() -> void:
 	if Open.disabled or not SteamManager.enabled:
 		return
-	Steam.getUserItemVote(id)
-	var ret: Array = await Steam.get_item_vote_result
-	if ret[0] == Steam.RESULT_OK:
+	SteamManager.steam.getUserItemVote(id)
+	var ret: Array = await SteamManager.steam.get_item_vote_result
+	if ret[0] == SteamManager.steam.RESULT_OK:
 		assert(ret[1] == id)
 		_on_vote_received(ret[2], ret[3], ret[4])
 
@@ -64,3 +64,21 @@ func _level_completed(info: Level.WinInfo) -> void:
 
 func _on_button_mouse_entered():
 	AudioManager.play_sfx("button_hover")
+
+
+func _on_thumb_toggled(now_pressed: bool, up: bool) -> void:
+	if not now_pressed:
+		# No going back
+		if up:
+			%ThumbUp.set_pressed_no_signal(true)
+		else:
+			%ThumbDown.set_pressed_no_signal(true)
+	else:
+		if up:
+			%ThumbDown.set_pressed_no_signal(false)
+		else:
+			%ThumbUp.set_pressed_no_signal(false)
+		SteamManager.steam.setUserItemVote(id, up)
+		var ret: Array = await SteamManager.steam.set_user_item_vote
+		if ret[0] != SteamManager.steam.RESULT_OK:
+			push_warning("Failed to vote")
