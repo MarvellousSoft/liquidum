1ccdafdac533dc6ab30c8b949b2146c3fc3b20bd
Yan Couto <yancouto@gmail.com>
2024-02-19 11:41:25 -0300

    Scale boot image to 4K
    closes #252

--- project/game/splash/SplashStaticImage.gd
+++ project/game/splash/SplashStaticImage.gd
@@ -4,6 +4,11 @@ extends Control
 func _ready() -> void:
 	Background.get_node("AnimationPlayer").play("RESET")
 	Background.get_node("Particles").emitting = false
+	var viewport := get_viewport()
+	if name.contains("Mobile"):
+		viewport.size = Vector2(720, 1280)
+	else:
+		viewport.size = Vector2(3840, 2160)
 	await get_tree().process_frame
 	await get_tree().process_frame
 	var img := get_viewport().get_texture().get_image()
