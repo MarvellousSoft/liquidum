6720e13a10b11617a6370e5a3e5f904372358ca5
Yan Soares Couto <yancouto@gmail.com>
2024-04-01 21:15:28 -0300

    Flair random color

--- project/game/flairs/FlairManager.gd
+++ project/game/flairs/FlairManager.gd
@@ -6,16 +6,17 @@ static func _monthly_flairs() -> Array[SelectableFlair]:
 	var data := UserData.current()
 	var cur_year := 2024
 	var cur_month := 1
+	var rng := RandomNumberGenerator.new()
 	for i in data.monthly_good_dailies.size():
 		cur_month += 1
 		if cur_month == 13:
 			cur_month = 1
 			cur_year += 1
 		if data.monthly_good_dailies[i] >= 15:
+			rng.seed = RandomHub.consistent_hash("%d-%d" % [cur_month, cur_year])
 			arr.append(SelectableFlair.new(
 				"pro",
-				# TODO: Random color
-				Color(0, 0, 0, 0),
+				Color.from_hsv(rng.randf(), 0.663, 0.804, 1),
 				S.tr("MONTHLY_FLAIR_DESC").format({
 					month=S.tr("MONTH_%02d" % [cur_month]),
 					year=cur_year,
