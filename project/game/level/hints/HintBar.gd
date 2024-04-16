2ee100b01764d1820dc57f455ac9a81f1036c3f6
Ricardo Lira <pudimdefrutas@gmail.com>
2024-04-16 14:02:26 -0300

    Fixed clicking on boat hint and filling with air

--- project/game/level/hints/HintBar.gd
+++ project/game/level/hints/HintBar.gd
@@ -45,7 +45,6 @@ func setup(grid, hints : Array, editor_mode : bool, swap_water_boat := false) ->
 		var water_hint = create_hint(grid, container, editor_mode, false, hints[i].water_count, hints[i].water_count_type)
 		boat_hint.mouse_entered.connect(_on_hint_mouse_entered.bind(i))
 		water_hint.mouse_entered.connect(_on_hint_mouse_entered.bind(i))
-		boat_hint.left_clicked.connect(_on_hint_left_clicked.bind(i))
 		water_hint.left_clicked.connect(_on_hint_left_clicked.bind(i))
 		if swap_water_boat:
 			boat_hint.move_to_front()
