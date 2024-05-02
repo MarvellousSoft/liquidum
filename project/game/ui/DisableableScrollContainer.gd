7194dacbf0683a7cc091d9684f557257d1dea80e
Yan Couto <yancouto@gmail.com>
2024-05-02 15:22:10 -0300

    Fix extra level hub scroll on mobile
    closes #460

--- project/game/ui/DisableableScrollContainer.gd
+++ project/game/ui/DisableableScrollContainer.gd
@@ -1,7 +1,14 @@
 class_name DisableableScrollContainer
 extends ScrollContainer
 
-var disable_scroll := false
+var disable_scroll: bool:
+	set(on):
+		disable_scroll = on
+		var mode := ScrollContainer.SCROLL_MODE_SHOW_NEVER if on else ScrollContainer.SCROLL_MODE_AUTO
+		vertical_scroll_mode = mode
+		horizontal_scroll_mode = mode
+	get:
+		return disable_scroll
 
 func _gui_input(_event):
 	if disable_scroll:
