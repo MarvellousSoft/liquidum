7352f1ff47c8cf82d2de9403444d238380f9c255
Ricardo Lira <pudimdefrutas@gmail.com>
2024-05-09 18:47:51 -0300

    Fix flair picker

--- project/game/main_menu/ModulateTextureButton.gd
+++ project/game/main_menu/ModulateTextureButton.gd
@@ -1,16 +1,26 @@
 class_name ModulateTextureButton
 extends Control
 
-const NORMAL_COLOR := Color(0.671, 1, 0.82)
-const HOVER_COLOR := Color(0.847, 0.996, 0.882)
+const NORMAL_COLOR := {
+	"dark": Color(0.671, 1, 0.82),
+	"normal": Color(0, 0.035, 0.141),
+}
+const HOVER_COLOR := {
+	"dark": Color(0.847, 0.996, 0.882),
+	"normal": Color(0.035, 0.212, 0.349),
+}
 
 func _ready():
-	modulate = NORMAL_COLOR
+	modulate = get_color(NORMAL_COLOR)
+
+
+func get_color(color):
+	return color.dark if Profile.get_option("dark_mode") else color.normal
 
 
 func _on_mouse_entered() -> void:
 	AudioManager.play_sfx("button_hover")
-	modulate = HOVER_COLOR
+	modulate = get_color(HOVER_COLOR)
 
 func _on_mouse_exited() -> void:
-	modulate = NORMAL_COLOR
+	modulate = get_color(NORMAL_COLOR)
